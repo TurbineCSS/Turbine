7c38ca727ae5eec1f1b419e9dafc07557945f827
Peter Kröner <peter@peterkroener.de>
2009-12-14 20:33:04 +0100

    No more notices when trying to find nonexisting "rounded"-properties
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- plugins/rounded_corner_shorthand.php
+++ plugins/rounded_corner_shorthand.php
@@ -16,7 +16,7 @@
 		foreach($parsed as $block => $css){
 			foreach($parsed[$block] as $selector => $styles){
 				// Everywhere
-				if($parsed[$block][$selector]['rounded']){
+				if(isset($parsed[$block][$selector]['rounded'])){
 					$value = $parsed[$block][$selector]['rounded'];
 					$parsed[$block][$selector]['-moz-border-radius'] = $value;
 					$parsed[$block][$selector]['-webkit-border-radius'] = $value;
@@ -24,7 +24,7 @@
 					unset($parsed[$block][$selector]['rounded']);
 				}
 				// Top only
-				if($parsed[$block][$selector]['rounded-top']){
+				if(isset($parsed[$block][$selector]['rounded-top'])){
 					$value = $parsed[$block][$selector]['rounded-top'];
 					$parsed[$block][$selector]['-moz-border-radius-topleft'] = $value;
 					$parsed[$block][$selector]['-moz-border-radius-topright'] = $value;
@@ -35,7 +35,7 @@
 					unset($parsed[$block][$selector]['rounded-top']);
 				}
 				// Bottom only
-				if($parsed[$block][$selector]['rounded-bottom']){
+				if(isset($parsed[$block][$selector]['rounded-bottom'])){
 					$value = $parsed[$block][$selector]['rounded-bottom'];
 					$parsed[$block][$selector]['-moz-border-radius-bottomleft'] = $value;
 					$parsed[$block][$selector]['-moz-border-radius-bottomright'] = $value;
@@ -46,7 +46,7 @@
 					unset($parsed[$block][$selector]['rounded-bottom']);
 				}
 				// Left only
-				if($parsed[$block][$selector]['rounded-left']){
+				if(isset($parsed[$block][$selector]['rounded-left'])){
 					$value = $parsed[$block][$selector]['rounded-left'];
 					$parsed[$block][$selector]['-moz-border-radius-topleft'] = $value;
 					$parsed[$block][$selector]['-moz-border-radius-bottomleft'] = $value;
@@ -57,7 +57,7 @@
 					unset($parsed[$block][$selector]['rounded-left']);
 				}
 				// Right only
-				if($parsed[$block][$selector]['rounded-right']){
+				if(isset($parsed[$block][$selector]['rounded-right'])){
 					$value = $parsed[$block][$selector]['rounded-right'];
 					$parsed[$block][$selector]['-moz-border-radius-topright'] = $value;
 					$parsed[$block][$selector]['-moz-border-radius-bottomright'] = $value;
@@ -68,7 +68,7 @@
 					unset($parsed[$block][$selector]['rounded-right']);
 				}
 				// Top left only
-				if($parsed[$block][$selector]['rounded-top-left']){
+				if(isset($parsed[$block][$selector]['rounded-top-left'])){
 					$value = $parsed[$block][$selector]['rounded-top-left'];
 					$parsed[$block][$selector]['-moz-border-radius-topleft'] = $value;
 					$parsed[$block][$selector]['-webkit-border-top-left-radius'] = $value;
@@ -76,7 +76,7 @@
 					unset($parsed[$block][$selector]['rounded-top-left']);
 				}
 				// Top right only
-				if($parsed[$block][$selector]['rounded-top-right']){
+				if(isset($parsed[$block][$selector]['rounded-top-right'])){
 					$value = $parsed[$block][$selector]['rounded-top-right'];
 					$parsed[$block][$selector]['-moz-border-radius-topright'] = $value;
 					$parsed[$block][$selector]['-webkit-border-top-right-radius'] = $value;
@@ -84,7 +84,7 @@
 					unset($parsed[$block][$selector]['rounded-top-right']);
 				}
 				// Bottom left only
-				if($parsed[$block][$selector]['rounded-bottom-left']){
+				if(isset($parsed[$block][$selector]['rounded-bottom-left'])){
 					$value = $parsed[$block][$selector]['rounded-bottom-left'];
 					$parsed[$block][$selector]['-moz-border-radius-bottomleft'] = $value;
 					$parsed[$block][$selector]['-webkit-border-bottom-left-radius'] = $value;
@@ -92,7 +92,7 @@
 					unset($parsed[$block][$selector]['rounded-bottom-left']);
 				}
 				// Bottom right only
-				if($parsed[$block][$selector]['rounded-bottom-right']){
+				if(isset($parsed[$block][$selector]['rounded-bottom-right'])){
 					$value = $parsed[$block][$selector]['rounded-bottom-right'];
 					$parsed[$block][$selector]['-moz-border-radius-bottomright'] = $value;
 					$parsed[$block][$selector]['-webkit-border-bottom-right-radius'] = $value;
