1d3836846a44ce9d29af874d4c01c5ebc642c77a
Peter Kröner <peter@peterkroener.de>
2009-12-17 20:51:36 +0100

    Os detector plugin basics working
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- plugins/os.php
+++ plugins/os.php
@@ -15,7 +15,10 @@
 	 * linux systems (^linux is overruled)
 	 * 
 	 * 
-	 * Status: -
+	 * Status: Alpha
+	 * @todo Implement version number matching for windows
+	 * @todo Implement multiple rules
+	 * @todo Walk the @font-face arrays too!
 	 * 
 	 * @param mixed &$parsed
 	 * @return void
@@ -24,7 +27,38 @@
 		global $browser;
 		foreach($parsed as $block => $css){
 			foreach($parsed[$block] as $selector => $styles){
-				// Do stuff
+				// Find os property
+				if($parsed[$block][$selector]['os']){
+					$remove = false;
+					$osrules = explode(' ', $parsed[$block][$selector]['os']);
+					foreach($osrules as $osrule){
+						$matches = array();
+						preg_match_all('/(\^)?(windows|mac|linux|unix)/i', $osrule, $matches);
+						// Os match?
+						if(strtolower($matches[2][0]) == strtolower($browser->platform)){
+							if($matches[1][0] == '^'){
+								$remove = true;
+							}
+							else{
+								$remove = false;
+							}
+						}
+						else{
+							if($matches[1][0] == '^'){
+								$remove = false;
+							}
+							else{
+								$remove = true;
+							}
+						}
+					}
+				}
+				if($remove == true){
+					unset($parsed[$block][$selector]);
+				}
+				else{
+					unset($parsed[$block][$selector]['os']);
+				}
 			}
 		}
 	}
