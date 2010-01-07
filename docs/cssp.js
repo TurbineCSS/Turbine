84e575c88b7b6d7a35099f58395cf6ce2d52fbfa
Peter Kröner <peter@peterkroener.de>
2010-01-07 19:25:18 +0100

    Updated docs
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- docs/cssp.js
+++ docs/cssp.js
@@ -1,10 +1,23 @@
 window.addEvent('domready', function(){
 
 	var toc = $$('#toc > ul')[0];
-	var tocLinks = toc.getElements('a')
 
+	// Smooth scroll
+	var tocLinks = toc.getElements('a');
 	var tocScroll = new Fx.SmoothScroll({
 		links: tocLinks
 	});
+
+	// Folding sub navigation
+	var subToc = toc.getElements('ul');
+	subToc.each(function(sub){
+		var parentLink = sub.getPrevious('a');
+		parentLink.addEvent('click', function(){
+			subToc.slide('out');
+			sub.slide('in');
+		});
+	});
+	subToc.slide('hide');
+	subToc[0].slide('in');
 
 });
--- roadmap.md
+++ roadmap.md
@@ -17,5 +17,5 @@ Known bugs:
 
  * [DONE] cssp.php: Aliased selectors are not inserted at their original position (L129)
  * cssp.php: Inheritance does not work for elements in arrays (L161)
- * parser.php: Fix sloppy merge (L446)
+ * [DONE] parser.php: Fix sloppy merge (L446)
  * [DONE] parser.php: Rewrite and unify merging of regular selectors and @font-face (L513-564)
