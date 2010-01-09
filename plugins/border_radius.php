c48f0dd1b63c20f418c5391c0292cf17ff7fd4d3
Peter Kröner <peter@peterkroener.de>
2010-01-09 13:03:03 +0100

    Added -cssp-prefix to box_shadow and border_radius plugins. Added -khtml variants of border radius to border_radius plugin.
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- plugins/border_radius.php
+++ plugins/border_radius.php
@@ -5,10 +5,11 @@
 	 * Adds vendor-specific versions of border-radius and adds border-top-radius,
 	 * border-bottom-radius, border-left-radius and border-right-radius
 	 * 
-	 * Usage: border-radius[-top/-bottom/-left/-right/-top-left/...]: value;
-	 * Example: border-top-radius:4px;
-	 * Example: border-bottom-left-radius:2em;
-	 * Status: Beta
+	 * Usage:     -cssp-border-radius[-top/-bottom/-left/-right/-top-left/...]: value;
+	 * Example 1: -cssp-border-top-radius:4px;
+	 * Example 2: -cssp-border-bottom-left-radius:2em;
+	 * Status:    Stable
+	 * Version:   1.0
 	 * 
 	 * @param mixed &$parsed
 	 * @return void
@@ -17,16 +18,20 @@
 		foreach($parsed as $block => $css){
 			foreach($parsed[$block] as $selector => $styles){
 				// Everywhere
-				if(isset($parsed[$block][$selector]['border-radius'])){
-					$value = $parsed[$block][$selector]['border-radius'];
+				if(isset($parsed[$block][$selector]['-ccsp-border-radius'])){
+					$value = $parsed[$block][$selector]['-ccsp-border-radius'];
 					$parsed[$block][$selector]['-moz-border-radius'] = $value;
+					$parsed[$block][$selector]['-khtml-border-radius'] = $value;
 					$parsed[$block][$selector]['-webkit-border-radius'] = $value;
+					$parsed[$block][$selector]['border-radius'] = $value;
 				}
 				// Top only
-				if(isset($parsed[$block][$selector]['border-top-radius'])){
-					$value = $parsed[$block][$selector]['border-top-radius'];
+				if(isset($parsed[$block][$selector]['-ccsp-border-top-radius'])){
+					$value = $parsed[$block][$selector]['-ccsp-border-top-radius'];
 					$parsed[$block][$selector]['-moz-border-radius-topleft'] = $value;
 					$parsed[$block][$selector]['-moz-border-radius-topright'] = $value;
+					$parsed[$block][$selector]['-khtml-border-top-left-radius'] = $value;
+					$parsed[$block][$selector]['-khtml-border-top-right-radius'] = $value;
 					$parsed[$block][$selector]['-webkit-border-top-left-radius'] = $value;
 					$parsed[$block][$selector]['-webkit-border-top-right-radius'] = $value;
 					$parsed[$block][$selector]['border-top-left-radius'] = $value;
@@ -34,10 +39,12 @@
 					unset($parsed[$block][$selector]['border-top-radius']);
 				}
 				// Bottom only
-				if(isset($parsed[$block][$selector]['border-bottom-radius'])){
-					$value = $parsed[$block][$selector]['border-bottom-radius'];
+				if(isset($parsed[$block][$selector]['-ccsp-border-bottom-radius'])){
+					$value = $parsed[$block][$selector]['-ccsp-border-bottom-radius'];
 					$parsed[$block][$selector]['-moz-border-radius-bottomleft'] = $value;
 					$parsed[$block][$selector]['-moz-border-radius-bottomright'] = $value;
+					$parsed[$block][$selector]['-khtml-border-bottom-left-radius'] = $value;
+					$parsed[$block][$selector]['-khtml-border-bottom-right-radius'] = $value;
 					$parsed[$block][$selector]['-webkit-border-bottom-left-radius'] = $value;
 					$parsed[$block][$selector]['-webkit-border-bottom-right-radius'] = $value;
 					$parsed[$block][$selector]['border-bottom-left-radius'] = $value;
@@ -45,10 +52,12 @@
 					unset($parsed[$block][$selector]['border-bottom-radius']);
 				}
 				// Left only
-				if(isset($parsed[$block][$selector]['border-left-radius'])){
-					$value = $parsed[$block][$selector]['border-left-radius'];
+				if(isset($parsed[$block][$selector]['-ccsp-border-left-radius'])){
+					$value = $parsed[$block][$selector]['-ccsp-border-left-radius'];
 					$parsed[$block][$selector]['-moz-border-radius-topleft'] = $value;
 					$parsed[$block][$selector]['-moz-border-radius-bottomleft'] = $value;
+					$parsed[$block][$selector]['-khtml-border-top-left-radius'] = $value;
+					$parsed[$block][$selector]['-khtml-border-bottom-left-radius'] = $value;
 					$parsed[$block][$selector]['-webkit-border-top-left-radius'] = $value;
 					$parsed[$block][$selector]['-webkit-border-bottom-left-radius'] = $value;
 					$parsed[$block][$selector]['border-top-left-radius'] = $value;
@@ -56,10 +65,12 @@
 					unset($parsed[$block][$selector]['border-left-radius']);
 				}
 				// Right only
-				if(isset($parsed[$block][$selector]['border-right-radius'])){
-					$value = $parsed[$block][$selector]['border-right-radius'];
+				if(isset($parsed[$block][$selector]['-ccsp-border-right-radius'])){
+					$value = $parsed[$block][$selector]['-ccsp-border-right-radius'];
 					$parsed[$block][$selector]['-moz-border-radius-topright'] = $value;
 					$parsed[$block][$selector]['-moz-border-radius-bottomright'] = $value;
+					$parsed[$block][$selector]['-khtml-border-top-right-radius'] = $value;
+					$parsed[$block][$selector]['-khtml-border-bottom-right-radius'] = $value;
 					$parsed[$block][$selector]['-webkit-border-top-right-radius'] = $value;
 					$parsed[$block][$selector]['-webkit-border-bottom-right-radius'] = $value;
 					$parsed[$block][$selector]['border-top-right-radius'] = $value;
@@ -67,28 +78,36 @@
 					unset($parsed[$block][$selector]['border-right-radius']);
 				}
 				// Top left only
-				if(isset($parsed[$block][$selector]['border-top-left-radius'])){
-					$value = $parsed[$block][$selector]['border-top-left-radius'];
+				if(isset($parsed[$block][$selector]['-ccsp-border-top-left-radius'])){
+					$value = $parsed[$block][$selector]['-ccsp-border-top-left-radius'];
 					$parsed[$block][$selector]['-moz-border-radius-topleft'] = $value;
+					$parsed[$block][$selector]['-khtml-border-top-left-radius'] = $value;
 					$parsed[$block][$selector]['-webkit-border-top-left-radius'] = $value;
+					$parsed[$block][$selector]['border-top-left-radius'] = $value;
 				}
 				// Top right only
-				if(isset($parsed[$block][$selector]['border-top-right-radius'])){
-					$value = $parsed[$block][$selector]['border-top-right-radius'];
+				if(isset($parsed[$block][$selector]['-ccsp-border-top-right-radius'])){
+					$value = $parsed[$block][$selector]['-ccsp-border-top-right-radius'];
 					$parsed[$block][$selector]['-moz-border-radius-topright'] = $value;
+					$parsed[$block][$selector]['-khtml-border-top-right-radius'] = $value;
 					$parsed[$block][$selector]['-webkit-border-top-right-radius'] = $value;
+					$parsed[$block][$selector]['border-top-right-radius'] = $value;
 				}
 				// Bottom left only
-				if(isset($parsed[$block][$selector]['border-bottom-left-radius'])){
-					$value = $parsed[$block][$selector]['border-bottom-left-radius'];
+				if(isset($parsed[$block][$selector]['-ccsp-border-bottom-left-radius'])){
+					$value = $parsed[$block][$selector]['-ccsp-border-bottom-left-radius'];
 					$parsed[$block][$selector]['-moz-border-radius-bottomleft'] = $value;
+					$parsed[$block][$selector]['-khtml-border-bottom-left-radius'] = $value;
 					$parsed[$block][$selector]['-webkit-border-bottom-left-radius'] = $value;
+					$parsed[$block][$selector]['border-bottom-left-radius'] = $value;
 				}
 				// Bottom right only
-				if(isset($parsed[$block][$selector]['border-bottom-right-radius'])){
-					$value = $parsed[$block][$selector]['border-bottom-right-radius'];
+				if(isset($parsed[$block][$selector]['-ccsp-border-bottom-right-radius'])){
+					$value = $parsed[$block][$selector]['-ccsp-border-bottom-right-radius'];
 					$parsed[$block][$selector]['-moz-border-radius-bottomright'] = $value;
+					$parsed[$block][$selector]['-khtml-border-bottom-right-radius'] = $value;
 					$parsed[$block][$selector]['-webkit-border-bottom-right-radius'] = $value;
+					$parsed[$block][$selector]['border-bottom-right-radius'] = $value;
 				}
 			}
 		}
--- plugins/box_shadow.php
+++ plugins/box_shadow.php
@@ -5,9 +5,10 @@
 	 * Easy box shadow
 	 * Adds vendor-specific versions of box-shadow
 	 * 
-	 * Usage: #foo { box-shadow: 2px 2px 8px #666; }
-	 * Result: #foo { box-shadow: 2px 2px 8px #666; -moz-box-shadow: 2px 2px 8px #666; -webkit-box-shadow: 2px 2px 8px #666; }
-	 * Status: Stable
+	 * Usage:   #foo { -cssp-box-shadow: 2px 2px 8px #666; }
+	 * Result:  #foo { -moz-box-shadow: 2px 2px 8px #666; -webkit-box-shadow: 2px 2px 8px #666; box-shadow: 2px 2px 8px #666; }
+	 * Status:  Stable
+	 * Version: 1.0
 	 * 
 	 * @param mixed &$parsed
 	 * @return void
@@ -15,10 +16,11 @@
 	function box_shadow(&$parsed){
 		foreach($parsed as $block => $css){
 			foreach($parsed[$block] as $selector => $styles){
-				if(isset($parsed[$block][$selector]['box-shadow'])){
-					$value = $parsed[$block][$selector]['box-shadow'];
+				if(isset($parsed[$block][$selector]['-cssp-box-shadow'])){
+					$value = $parsed[$block][$selector]['-cssp-box-shadow'];
 					$parsed[$block][$selector]['-moz-box-shadow'] = $value;
 					$parsed[$block][$selector]['-webkit-box-shadow'] = $value;
+					$parsed[$block][$selector]['box-shadow'] = $value;
 				}
 			}
 		}
