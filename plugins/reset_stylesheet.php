06e4f8b20e97f0b8d37509dbc7354c3652818401
Peter Kröner <peter@peterkroener.de>
2010-01-18 14:22:22 +0100

    Fixed reset plugin
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- plugins/reset_stylesheet.php
+++ plugins/reset_stylesheet.php
@@ -12,13 +12,13 @@
 	 */
 	function reset_stylesheet(&$parsed){
 		// The reset stylesheet
-		$reset_stylesheet = '*{margin:0;padding:0;text-decoration:none;border:0;outline:0;font-weight:normal;font-style:inherit;font-size:100%;font-family:inherit;vertical-align:baseline;line-height:inherit;color:inherit;background:none;text-align:inherit;quotes:"""";list-style:inherit;border-collapse:collapse;border-spacing:0;}';
+		$reset_stylesheet = "*\n	margin:0;padding:0;text-decoration:none;border:0;outline:0;font-weight:normal;font-style:inherit;font-size:100%;font-family:inherit;vertical-align:baseline;line-height:inherit;color:inherit;background:none;text-align:inherit;quotes:'''';list-style:inherit;border-collapse:collapse;border-spacing:0";
 		// Parse the stylesheet
 		$cssp = new Cssp();
 		$cssp->load_string($reset_stylesheet);
 		$cssp->parse();
 		// Merge the reset array into the existing $parsed
-		$parsed['css'] = array_merge($cssp->parsed['css'], $parsed['css']);
+		$parsed['global'] = array_merge($cssp->parsed['global'], $parsed['global']);
 	}
 
 
