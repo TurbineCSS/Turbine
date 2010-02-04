206c0ecf019452308e13af39428a76a21d721a1c
Peter Kröner <peter@peterkroener.de>
2010-02-04 21:20:33 +0100

    Colortools now accepts shorthand hex values and processes box-shadow and text-shadow
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- plugins/colortools.php
+++ plugins/colortools.php
@@ -15,7 +15,7 @@
 	 */
 	function colortools(&$parsed){
 		$properties = array('background', 'background-color', 'color', 'border', 'border-color',
-			'border-top', 'border-left', 'border-bottom', 'border-right');
+			'border-top', 'border-left', 'border-bottom', 'border-right, text-shadow, box-shadow');
 		foreach($properties as $search){
 			foreach($parsed as $block => $css){
 				foreach($parsed[$block] as $selector => $styles){
@@ -33,7 +33,6 @@
 	/**
 	 * colortools_parse_expressions
 	 * Callback for managing the different color operations
-	 * 
 	 * @param array $matches The values returned by preg_replace_callback
 	 * @return string The result of the colortools_* operation
 	 */
@@ -45,7 +44,6 @@
 	/**
 	 * colortools_darker
 	 * Darkens $color by $percentage
-	 * 
 	 * @param string $color The color to darken
 	 * @package int $percentage The percentage to darken by
 	 */
@@ -63,7 +61,6 @@
 	/**
 	 * colortools_lighter
 	 * Lightens $color by $percentage
-	 * 
 	 * @param string $color The color to darken
 	 * @package int $percentage The percentage to darken by
 	 */
@@ -87,6 +84,9 @@
 	 * @return array $rgb The RGB values
 	 */
 	function colortools_hexrgb($hex){
+		if(strlen($hex) == 4){
+			$hex = '#'.$hex{1}.$hex{1}.$hex{2}.$hex{2}.$hex{3}.$hex{3};
+		}
 		$int = hexdec($hex);
 		$rgb = array('r' => 0xFF & ($int >> 0x10), 'g' => 0xFF & ($int >> 0x8), 'b' => 0xFF & $int);
 		return $rgb;
