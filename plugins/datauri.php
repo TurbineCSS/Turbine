2b0d36eba603534ddcdd41c342b53eeeae222d69
Christian Schepp Schaefer <schaepp@gmx.de>
2010-02-07 20:55:40 +0100

    Added MHTML-Support for datauri-plugin
    Signed-off-by: Christian Schepp Schaefer <schaepp@gmx.de>

--- plugins/datauri.php
+++ plugins/datauri.php
@@ -14,31 +14,70 @@
 	function datauri(&$parsed){
 		global $browser;
 		global $file;
+		$mhtmlmd5 = md5(
+			$browser->platform.
+			$browser->platformversion.
+			$browser->platformtype.
+			$browser->engine.
+			$browser->engineversion.
+			$browser->family.
+			$browser->familyversion.
+			$browser->name.
+			$browser->version.
+			$file
+		);
+		// Start preparing MHTML
+		$mhtmlfile = 'lib/cssp_cache/'.$mhtmlmd5.'_mhtml.txt';
+		$mhtmlarray = array();
+		$mhtmlcontent = "Content-Type: multipart/related; boundary=\"_ANY_STRING_WILL_DO_AS_A_SEPARATOR\"\r\n\r\n";
 		foreach($parsed as $block => $css){
 			foreach($parsed[$block] as $selector => $styles){
 				// Check for backgrounds
 				if(isset($parsed[$block][$selector]['background']) || isset($parsed[$block][$selector]['background-image'])){
-					if(
-						($browser->family == 'MSIE' && floatval($browser->familyversion) >= 8) || 
-						$browser->family == 'Firefox' || 
-						$browser->family == 'Opera' || 
-						$browser->family == 'WebKit' || 
-						$browser->family == 'KHTML'
-					){
-						$regex = '/(url\()[\'"]*([^\'"\)]+)[\'"]*(\))/i';
-						$properties = array('background','background-image','src');
-						$basedirectories = array(dirname($file),str_replace('\\','/',dirname(realpath($_SERVER['SCRIPT_FILENAME']))),str_replace('\\','/',dirname(__FILE__)));
-						foreach($properties as $property){
-							foreach($basedirectories as $basedirectory){
-								if(isset($parsed[$block][$selector][$property])){
-									if(!is_array($parsed[$block][$selector][$property])){ // Ignore multi-value-properties so we don't mess with other plugins
-										if(preg_match($regex, $parsed[$block][$selector][$property], $matches) > 0){
-											$imagefile = $basedirectory.'/'.$matches[2];
-											if(file_exists($imagefile) && filesize($imagefile) <= 24000){
-												$pathinfo = pathinfo($imagefile);
-												$imagetype = strtolower($pathinfo['extension']);
-												$imagedata = base64_encode(file_get_contents($imagefile));
+					$regex = '/(url\()[\'"]*([^\'"\)]+)[\'"]*(\))/i';
+					$properties = array('background','background-image','src');
+					$basedirectories = array(dirname($file),str_replace('\\','/',dirname(realpath($_SERVER['SCRIPT_FILENAME']))),str_replace('\\','/',dirname(__FILE__)));
+					foreach($properties as $property){
+						foreach($basedirectories as $basedirectory){
+							if(isset($parsed[$block][$selector][$property])){
+								if(!is_array($parsed[$block][$selector][$property])){ // Ignore multi-value-properties so we don't mess with other plugins
+									if(preg_match($regex, $parsed[$block][$selector][$property], $matches) > 0){
+										$imagefile = $basedirectory.'/'.$matches[2];
+										if(file_exists($imagefile) && filesize($imagefile) <= 24000){
+											$pathinfo = pathinfo($imagefile);
+											$imagetype = strtolower($pathinfo['extension']);
+											$imagedata = base64_encode(file_get_contents($imagefile));
+											if(
+												($browser->family == 'MSIE' && floatval($browser->familyversion) >= 8) || 
+												$browser->engine == 'Gecko' || 
+												$browser->family == 'Opera' || 
+												$browser->family == 'WebKit' || 
+												$browser->family == 'KHTML'
+											){
 												$parsed[$block][$selector][$property] = preg_replace($regex, '$1\'data:image/'.$imagetype.';base64,'.$imagedata.'\'$3', $parsed[$block][$selector][$property]);
+											}
+											elseif(
+												$browser->family == 'MSIE' && 
+												(
+													floatval($browser->familyversion) < 7 || 
+													(floatval($browser->familyversion) < 8 && $this->platform && $this->platformversion < 6)
+												) 
+											){
+												// Calculate identifier and anchor-tag for the MHTML-file
+												$imagetag = 'img'.md5($imagefile);
+												// Look up in our list if we did not already process that exact file, if not append it
+												if(!isset($mhtmlarray[$imagetag])) 
+												{
+													$mhtmlcontent .= "--_ANY_STRING_WILL_DO_AS_A_SEPARATOR\r\n";
+													$mhtmlcontent .= "Content-Location:".$imagetag."\r\n";
+													$mhtmlcontent .= "Content-Transfer-Encoding:base64\r\n\r\n";
+													$mhtmlcontent .= base64_encode(file_get_contents($imagefile))."==\r\n";
+													
+													// Put file on our processed-list
+													$mhtmlarray[$imagetag] = 1;
+												}
+												
+												$parsed[$block][$selector][$property] = preg_replace($regex, '$1\'mhtml:'.($_SERVER['HTTPS'] ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST'].rtrim(dirname($_SERVER['SCRIPT_NAME']),'/').'/plugins/datauri/mhtml.php?cache='.$mhtmlmd5.'!'.$imagetag.'\'$3', $parsed[$block][$selector][$property]);
 											}
 										}
 									}
@@ -49,6 +88,10 @@
 				}
 			}
 		}
+		$mhtmlcontent .= "\r\n\r\n";
+		// Store the MHTML cache-file
+		@file_put_contents($mhtmlfile,$mhtmlcontent);
+		@chmod($mhtmlfile,0777);
 	}
 
 
