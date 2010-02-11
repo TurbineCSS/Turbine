b2d205b086fadd68cfba0a52035555a45ed486f5
Christian Schepp Schaefer <schaepp@gmx.de>
2010-02-11 13:23:36 +0100

    Created a transition-plugin that adds vendor-specific transition-properties to your styles
    Signed-off-by: Christian Schepp Schaefer <schaepp@gmx.de>

---
+++ plugins/transition.php
@@ -0,0 +1,50 @@
+<?php
+
+	/**
+	 * Easy and extended transition
+	 * Adds vendor-specific versions of transition and adds border-top-radius,
+	 * border-bottom-radius, border-left-radius and border-right-radius
+	 * 
+	 * Usage:     Use any currently known trnasition-property wihtout vendor-prefixes
+	 * Example 1: transition: left 1s linear;
+	 * Example 2: transition-property: background-color; transition-duration: 4s;
+	 * Status:    Stable
+	 * Version:   1.0
+	 * 
+	 * @param mixed &$parsed
+	 * @return void
+	 */
+	function transition(&$parsed){
+		$transition_properties = array(
+		'transition',
+		'transition-property',
+		'transition-duration',
+		'transition-delay',
+		'transition-timing-function'
+		);
+		
+		foreach($parsed as $block => $css){
+			foreach($parsed[$block] as $selector => $styles){
+				$parsed[$block][$selector]['arraylength'] = count($transition_properties);
+				foreach($transition_properties as $property)
+				{
+					$parsed[$block][$selector]['schepp'] = $property;
+					if(isset($parsed[$block][$selector][$property])){
+						$value = $parsed[$block][$selector][$property];
+						$parsed[$block][$selector]['-moz-'.$property] = $value;
+						$parsed[$block][$selector]['-o-'.$property] = $value;
+						$parsed[$block][$selector]['-webkit-'.$property] = $value;
+					}
+				}
+			}
+		}
+	}
+
+
+	/**
+	 * Register the plugin
+	 */
+	$cssp->register_plugin('before_compile', 0, 'transition');
+
+
+?>
