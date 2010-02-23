8a24afe3127bc9520ac6f012be11fd5979037b95
Christian Schepp Schaefer <schaepp@gmx.de>
2010-02-23 18:50:33 +0100

    Renamed plugin html5elements to html5
    Signed-off-by: Christian Schepp Schaefer <schaepp@gmx.de>

--- plugins/html5.php
+++ plugins/html5.php
@@ -18,7 +18,7 @@
 			{
 				// Registration of HTML5 elements
 				// Fix inspired by http://code.google.com/p/html5shiv/
-				$htc_path = rtrim(dirname($_SERVER['SCRIPT_NAME']),'/').'/plugins/html5elements/html5elements.htc';
+				$htc_path = rtrim(dirname($_SERVER['SCRIPT_NAME']),'/').'/plugins/html5/html5.htc';
 				if(!isset($parsed[$block]['body'])) $parsed[$block]['body'] = array();	
 				if(!isset($parsed[$block]['body']['behavior'])) $parsed[$block]['body']['behavior'] = 'url("'.$htc_path.'")';
 				else if(!strpos($parsed[$block]['body']['behavior'],'url("'.$htc_path.'")')) $parsed[$block]['body']['behavior'] .= ', url("'.$htc_path.'")';
