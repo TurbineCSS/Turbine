4729af65c1415b337406b3098d9f774424b82e4e
Peter Kröner <peter@peterkroener.de>
2010-02-26 15:54:55 +0100

    Minifier now optimized tokenized strings like font-family
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- plugins/minifier.php
+++ plugins/minifier.php
@@ -7,13 +7,14 @@
 	 * Usage: Nobrainer, just switch it on
 	 * Status: Beta
 	 * 
+	 * @todo: Optimize stuff like margin: 4px 4px 4px 4px;
+	 * @todo: Remove units from zero-values like 0em
 	 * @param mixed &$parsed
 	 * @return void
 	 */
 	function minifier(&$parsed){
-		global $browser;
-		// Hex colors
-		$pattern = '/#([A-F0-9])\1([A-F0-9])\2([A-F0-9])\3/i';
+		global $browser, $cssp;
+		$colorpattern = '/#([A-F0-9])\1([A-F0-9])\2([A-F0-9])\3/i';
 		$colorproperties = array(
 			'color',
 			'background',
@@ -25,13 +26,23 @@
 			'border-bottom',
 			'border-right'
 		);
-		// TODO: Optimize stuff like margin: 4px 4px 4px 4px;
+		$tokenizedproperties = array(
+			'font-family'
+		);
 		foreach($parsed as $block => $css){
 			foreach($parsed[$block] as $selector => $styles){
-				foreach($colorproperties as $colorproperty){
-					if(isset($parsed[$block][$selector][$colorproperty])){
-						$parsed[$block][$selector][$colorproperty] =
-							preg_replace($pattern, '#\1\2\3', $parsed[$block][$selector][$colorproperty]);
+				// Optimize hex colors
+				foreach($colorproperties as $property){
+					if(isset($parsed[$block][$selector][$property])){
+						$parsed[$block][$selector][$property] =
+							preg_replace($colorpattern, '#\1\2\3', $parsed[$block][$selector][$property]);
+					}
+				}
+				// Optimize tokenized strings
+				foreach($tokenizedproperties as $property){
+					if(isset($parsed[$block][$selector][$property])){
+						$parsed[$block][$selector][$property] =
+							implode(',', $cssp->tokenize($parsed[$block][$selector][$property], ','));
 					}
 				}
 			}
