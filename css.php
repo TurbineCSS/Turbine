0327abbe9ab26379e5b55ed07adc79f3dda47adb
Peter Kröner <peter@peterkroener.de>
2010-02-26 10:25:47 +0100

    Configuration rule now is "@turbine"
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- css.php
+++ css.php
@@ -195,7 +195,7 @@ if($_GET['files']){
 					$found = false;
 					foreach($cssp->css as $line){
 						if(!$found){
-							if(preg_match('/^[\s\t]*@cssp/i',$line) == 1){
+							if(preg_match('/^[\s\t]*@turbine/i',$line) == 1){
 								$found = true;
 							}
 						}
@@ -217,15 +217,15 @@ if($_GET['files']){
 
 
 					// Set compression mode
-					if(isset($cssp->parsed['global']['@cssp']['compress'])){
-						$compress = (bool) $cssp->parsed['global']['@cssp']['compress'];
+					if(isset($cssp->parsed['global']['@turbine']['compress'])){
+						$compress = (bool) $cssp->parsed['global']['@turbine']['compress'];
 					}
 					else{
 						$compress = false;
 					}
 
 
-					unset($cssp->parsed['global']['@cssp']);                      // Remove configuration @-rule
+					unset($cssp->parsed['global']['@turbine']);                      // Remove configuration @-rule
 					$output = $cssp->glue($compress);                             // Glue css output
 					$cssp->apply_plugins('before_output', $plugin_list, $output); // Apply plugins for before output
 
