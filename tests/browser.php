c85f7348a343b09d5036d6a7cff55efbf68d5f04
Peter Kröner <peter@peterkroener.de>
2010-03-26 12:18:26 +0100

    Added a lot of new browser test cases
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- tests/browser.php
+++ tests/browser.php
@@ -28,6 +28,16 @@ $ua_tests = array(
 		)
 	),
 	'IE8 on Win7' => array(
+		'ua' => 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; MS-RTC LM 8)',
+		'test' => array(
+			'name' => 'msie',
+			'version' => '8',
+			'engine' => 'msie',
+			'engineversion' => '8',
+			'platform' => 'windows',
+			'platformversion' => '6.1',
+			'platformtype' => 'desktop'
+		)
 	),
 	'IE8 on Vista' => array(
 		'ua' => 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)',
@@ -42,6 +52,16 @@ $ua_tests = array(
 		)
 	),
 	'IE8 on WinXP' => array(
+		'ua' => 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; InfoPath.1; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; .NET CLR 3.0.04506.648; yie8)',
+		'test' => array(
+			'name' => 'msie',
+			'version' => '8',
+			'engine' => 'msie',
+			'engineversion' => '8',
+			'platform' => 'windows',
+			'platformversion' => '5.1',
+			'platformtype' => 'desktop'
+		)
 	),
 	'IE7 on Win7' => array(
 	),
@@ -66,10 +86,69 @@ $ua_tests = array(
 
 
 	// Gecko/Mozilla
-	
+	'Firefox 3.6 on Win7' => array(
+		'ua' => '# Mozilla/5.0 (Windows; U; Windows NT 6.1; lt; rv:1.9.2) Gecko/20100115 Firefox/3.6',
+		'test' => array(
+			'name' => 'gecko',
+			'version' => '1.923',
+			'engine' => 'gecko',
+			'engineversion' => '1.92',
+			'platform' => 'windows',
+			'platformversion' => '6.1',
+			'platformtype' => 'desktop'
+		)
+	),
+	'Firefox 3.6 on Vista' => array(
+		'ua' => 'Mozilla/5.0 (Windows; U; Windows NT 6.0; ru; rv:1.9.2) Gecko/20100115 Firefox/3.6',
+		'test' => array(
+			'name' => 'gecko',
+			'version' => '1.923',
+			'engine' => 'gecko',
+			'engineversion' => '1.92',
+			'platform' => 'windows',
+			'platformversion' => '6.0',
+			'platformtype' => 'desktop'
+		)
+	),
+	'Firefox 3.6 on WinXP' => array(
+		'ua' => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; pt-BR; rv:1.9.2) Gecko/20100115 Firefox/3.6',
+		'test' => array(
+			'name' => 'gecko',
+			'version' => '1.923',
+			'engine' => 'gecko',
+			'engineversion' => '1.92',
+			'platform' => 'windows',
+			'platformversion' => '5.1',
+			'platformtype' => 'desktop'
+		)
+	),
 
 
-	// Strange Gecko/Mozilla spin-offs
+	// Strange Gecko/Mozilla pre-releases and spin-offs
+	'Firefox 3.6.x pre on Linux' => array(
+		'ua' => 'Mozilla/5.0 (X11; U; Linux x86_64; de-DE; rv:1.9.2.3pre) Gecko/20100326 Ubuntu/9.10 (karmic) Namoroka/3.6.3pre',
+		'test' => array(
+			'name' => 'gecko',
+			'version' => '1.923',
+			'engine' => 'gecko',
+			'engineversion' => '1.923',
+			'platform' => 'linux',
+			'platformversion' => '0',
+			'platformtype' => 'desktop'
+		)
+	),
+	'Firefox 3.7a pre on Linux' => array(
+		'ua' => 'Mozilla/5.0 (X11; U; Linux x86_64; de-DE; rv:1.9.3a3pre) Gecko/20100304 Ubuntu/9.10 (karmic) Minefield/3.7a3pre',
+		'test' => array(
+			'name' => 'gecko',
+			'version' => '1.93',
+			'engine' => 'gecko',
+			'engineversion' => '1.93',
+			'platform' => 'linux',
+			'platformversion' => '0',
+			'platformtype' => 'desktop'
+		)
+	),
 	'Songbird 1.4 on Linux' => array(
 		'ua' => 'Mozilla/5.0 (X11; U; Linux x86_64; de; rv:1.9.0.14) Gecko/2009091418 Songbird/1.4.3 (20091223030122)',
 		'test' => array(
@@ -139,12 +218,52 @@ $ua_tests = array(
 
 	// Chrome
 	'Chrome 5 on Win 7' => array(
+		'ua' => 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.3 (KHTML, like Gecko) Chrome/5.0.354.0 Safari/533.3',
+		'test' => array(
+			'name' => 'chrome',
+			'version' => '5.0',
+			'engine' => 'webkit',
+			'engineversion' => '533.3',
+			'platform' => 'windows',
+			'platformversion' => '6.1',
+			'platformtype' => 'desktop'
+		)
 	),
 	'Chrome 5 on Vista' => array(
+		'ua' => 'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/533.2 (KHTML, like Gecko) Chrome/5.0.342.5 Safari/533.2',
+		'test' => array(
+			'name' => 'chrome',
+			'version' => '5.0',
+			'engine' => 'webkit',
+			'engineversion' => '533.2',
+			'platform' => 'windows',
+			'platformversion' => '6.0',
+			'platformtype' => 'desktop'
+		)
 	),
 	'Chrome 5 on WinXP' => array(
+		'ua' => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/533.3 (KHTML, like Gecko) Chrome/5.0.355.0 Safari/533.3',
+		'test' => array(
+			'name' => 'chrome',
+			'version' => '5.0',
+			'engine' => 'webkit',
+			'engineversion' => '533.3',
+			'platform' => 'windows',
+			'platformversion' => '5.1',
+			'platformtype' => 'desktop'
+		)
 	),
 	'Chrome 5 on OS X' => array(
+		'ua' => 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_2; en-US) AppleWebKit/533.2 (KHTML, like Gecko) Chrome/5.0.343.0 Safari/533.2',
+		'test' => array(
+			'name' => 'chrome',
+			'version' => '5.0',
+			'engine' => 'webkit',
+			'engineversion' => '533.2',
+			'platform' => 'macintosh',
+			'platformversion' => '0',
+			'platformtype' => 'desktop'
+		)
 	),
 	'Chrome 5 on Linux' => array(
 		'ua' => 'Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/532.9 (KHTML, like Gecko) Chrome/5.0.307.11 Safari/532.9',
@@ -159,6 +278,8 @@ $ua_tests = array(
 		)
 	),
 
+
+	// The more esoteric browsers
 	'iCab >= 4' => array(
 		'ua' => 'iCab/4.5 (Macintosh; U; Mac OS X Leopard 10.5.7)',
 		'test' => array(
@@ -171,7 +292,6 @@ $ua_tests = array(
 			'platformtype' => 'desktop'
 		)
 	),
-
 	'iCab < 4' => array(
 		'ua' => 'Mozilla/5.0 (compatible; iCab 3.0.5; Macintosh; U; PPC Mac OS)',
 		'test' => array(
@@ -184,6 +304,11 @@ $ua_tests = array(
 			'platformtype' => 'desktop'
 		)
 	)
+
+
+	// Mobile browsers
+	
+
 );
 
 ?>
@@ -221,7 +346,7 @@ $properties = array('name', 'version', 'engine', 'engineversion', 'platform', 'p
 
 foreach($ua_tests as $title => $test){
 	echo '<tr>';
-	echo '<th>'.$title.'</th>';
+	echo '<th id="'.preg_replace('/[^(\x20-\x7F)\x0A]*/','', $title).'"><a href="#'.preg_replace('/[^(\x20-\x7F)\x0A]*/','', $title).'">'.$title.'</a></th>';
 	$browser = new Browser($test['ua']);
 	foreach($properties as $property){
 		if(!isset($test['test'][$property]) || $test['test'][$property] == ''){
