5284fd0e5c3786079bf88ef73b08da6eb2dc0f71
Peter Kröner <peter@peterkroener.de>
2010-04-06 12:11:14 +0200

    Added iPad test case
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- tests/browser.php
+++ tests/browser.php
@@ -545,6 +545,19 @@ $ua_tests = array(
 		)
 	),
 
+	// Tablets
+	'iPad' => array(
+		'ua' => 'Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10',
+		'test' => array(
+			'name' => 'safari',
+			'version' => '4.04',
+			'engine' => 'webkit',
+			'engineversion' => '531.211',
+			'platform' => 'macintosh',
+			'platformversion' => '0',
+			'platformtype' => 'desktop'
+		)
+	),
 
 	// Mobile browsers
 	'iPhone OS 3.0' => array(
