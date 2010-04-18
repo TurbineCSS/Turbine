9ce449b57f788bce892f0081914042eda41cba35
Peter Kröner <peter@peterkroener.de>
2010-04-18 22:23:48 +0200

    Bugfix plugin: global bugfixes work
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- plugins/bugfixes.php
+++ plugins/bugfixes.php
@@ -12,61 +12,37 @@
 	 * @return void
 	 */
 	function bugfixes(&$parsed){
-		global $browser;
-		foreach($parsed as $block => $css){
+		global $cssp, $browser;
+		$changed = array();
 
-			// IE 6 global bugfixes
-			if($browser->family == 'MSIE' && floatval($browser->familyversion) < 7){
-				// Image margin bottom bug
-				if(!isset($parsed[$block]['img'])){
-					$parsed[$block]['img'] = array();
-				}
-				$parsed[$block]['img']['vertical-align'] = 'bottom';
-				CSSP::comment($parsed[$block]['img'], 'vertical-align', 'Added by bugfix plugin');
-				// Background image flickers on hover
-				if(!isset($parsed[$block]['html'])){
-					$parsed[$block]['html'] = array();
-				}
-				if(!isset($parsed[$block]['html']['filter'])){
-					$parsed[$block]['html']['filter'] = 'expression(document.execCommand("BackgroundImageCache",false,true))';
-					CSSP::comment($parsed[$block]['html'], 'filter', 'Added by bugfix plugin');
-				}
-				else{
-					if(!strpos($parsed[$block]['html']['filter'], 'expression(document.execCommand("BackgroundImageCache",false,true))')){
-						$parsed[$block]['html']['filter'] .= ' expression(document.execCommand("BackgroundImageCache",false,true))';
-						CSSP::comment($parsed[$block]['html'], 'filter', 'Modified by bugfix plugin');
-					}
-				}
-			}
+		// IE 6 global bugfixes
+		if($browser->engine == 'MSIE' && floatval($browser->engineversion) < 7){
+			// Image margin bottom bug
+			$changed['img']['vertical-align'][] = 'bottom';
+			// Background image flickers on hover
+			$changed['html']['filter'][] = 'expression(document.execCommand("BackgroundImageCache",false,true))';
+		}
 
-			// IE 6 + 7 global bugfixes
-			if($browser->family == 'MSIE' && floatval($browser->familyversion) < 8){
-				// Enable full styleability for IE-buttons
-				// See http://www.sitepoint.com/forums/showthread.php?t=547059
-				if(!isset($parsed[$block]['button'])){
-					$parsed[$block]['button'] = array();
-				}
-				$parsed[$block]['button']['overflow'] = 'visible';
-				$parsed[$block]['button']['width'] = 'auto';
-				$parsed[$block]['button']['white-space'] = 'nowrap';
-				CSSP::comment($parsed[$block]['button'], 'overflow', 'Added by bugfix plugin');
-				CSSP::comment($parsed[$block]['button'], 'width', 'Added by bugfix plugin');
-				CSSP::comment($parsed[$block]['button'], 'white-space', 'Added by bugfix plugin');
-			}
+		// IE 6 + 7 global bugfixes
+		if($browser->engine == 'MSIE' && floatval($browser->engineversion) < 8){
+			// Enable full styleability for IE-buttons, see http://www.sitepoint.com/forums/showthread.php?t=547059
+			$changed['button']['overflow'][] = 'visible';
+			$changed['button']['width'][] = 'auto';
+			$changed['button']['white-space'][] = 'nowrap';
+		}
 
-			// Firefox global bugfixes
-			if($browser->engine == 'Gecko'){
-				// Ghost margin around buttons
-				// See http://www.sitepoint.com/forums/showthread.php?t=547059
-				if(!isset($parsed[$block]['button::-moz-focus-inner'])){
-					$parsed[$block]['button::-moz-focus-inner'] = array();
-				}
-				$parsed[$block]['button::-moz-focus-inner']['padding'] = '0';
-				$parsed[$block]['button::-moz-focus-inner']['border'] = 'none';
-				CSSP::comment($parsed[$block]['button::-moz-focus-inner'], 'padding', 'Added by bugfix plugin');
-				CSSP::comment($parsed[$block]['button::-moz-focus-inner'], 'border', 'Added by bugfix plugin');
-			}
+		// Firefox global bugfixes
+		if($browser->engine == 'Gecko'){
+			// Ghost margin around buttons, see http://www.sitepoint.com/forums/showthread.php?t=547059
+			$changed['button::-moz-focus-inner']['padding'][] = '0';
+			$changed['button::-moz-focus-inner']['border'][] = 'none';
+		}
 
+		// Insert the global bugfixes
+		$cssp->insert($changed, 'global');
+		// print_r($changed);
+
+/*
 			foreach($parsed[$block] as $selector => $styles){
 
 				// IE 6 local bugfixes
@@ -96,7 +72,7 @@
 
 			}
 
-		}
+		}*/
 	}
 
 
