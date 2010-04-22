071fe1d4a776a6f672653e760245111b1ca4686f
Peter Kröner <peter@peterkroener.de>
2010-04-22 17:21:45 +0200

    Added docs for multi-line selectors
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- lib/parser.php
+++ lib/parser.php
@@ -25,7 +25,6 @@
 /**
  * Parser2
  * Turbine syntax parser
- * @todo document schepp-style selectors
  * @todo document inline css (@css)
  * @todo document the comment() method
  * @todo document the fact that values are now always arrays
