a76bc9b7f3826d721bfc5d7db7f83fc01de32a38
Peter Kröner <peter@peterkroener.de>
2010-04-28 23:35:53 +0200

    IE enhancements plugin: added opacity support for IE 6 and 7
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- plugins/ieenhancements.php
+++ plugins/ieenhancements.php
@@ -2,24 +2,42 @@
 
 
 	/**
-	 * A bunch of general IE enhancements
+	 * IE enhancements
+	 * Enables a bunch of usually absent features in IE 6 and 7
 	 * 
 	 * Usage: Nobrainer, just switch it on
 	 * Example: -
-	 * Status: Beta
+	 * Status: Stable
 	 * 
 	 * @param mixed &$parsed
 	 * @return void
 	 */
-	function ie6enhancements(&$parsed){
-		global $browser;
-		if($browser->engine == 'MSIE' && floatval($browser->engineversion) < 7){
-			// Missing :hover-property on every tag except link-tag, see http://www.xs4all.nl/~peterned/csshover.html
-			$htc_path = rtrim(dirname($_SERVER['SCRIPT_NAME']),'/').'/plugins/ieenhancements/csshover3.htc';
-			$parsed['global']['body']['behavior'][] = 'url("'.$htc_path.'")';
-			// Fix transparent PNGs, see http://www.twinhelix.com/css/iepngfix/
-			$htc_path = rtrim(dirname($_SERVER['SCRIPT_NAME']),'/').'/plugins/ieenhancements/iepngfix.htc';
-			$parsed['global']['img']['behavior'][] = 'url("'.$htc_path.'")';
+	function ieenhancements(&$parsed){
+		global $browser, $cssp;
+		if($browser->engine == 'MSIE'){
+			// Fixes for IE 6 and 7
+			if(floatval($browser->engineversion) < 8){
+				// Enable opacity through a proprietary filter
+				foreach($parsed as $block => $css){
+					foreach($parsed[$block] as $selector => $styles){
+						if(isset($parsed[$block][$selector]['opacity'])){
+							$value = $cssp->get_final_value($parsed[$block][$selector]['opacity'], 'opacity');
+							$filter_value = 100 * floatval($value);
+							$parsed[$block][$selector]['filter'][] = 'alpha(opacity='.$filter_value.')';
+							$parsed[$block][$selector]['zoom'][] = '1';
+						}
+					}
+				}
+				// Fixes for IE 6 only
+				if(floatval($browser->engineversion) < 7){
+					// Missing :hover-property on every tag except link-tag, see http://www.xs4all.nl/~peterned/csshover.html
+					$htc_path = trim(dirname($_SERVER['SCRIPT_NAME']),'/').'/plugins/ieenhancements/csshover3.htc';
+					$parsed['global']['body']['behavior'][] = 'url("'.$htc_path.'")';
+					// Fix transparent PNGs, see http://www.twinhelix.com/css/iepngfix/
+					$htc_path = trim(dirname($_SERVER['SCRIPT_NAME']),'/').'/plugins/ieenhancements/iepngfix.htc';
+					$parsed['global']['img']['behavior'][] = 'url("'.$htc_path.'")';
+				}
+			}
 		}
 	}
 
@@ -27,7 +45,7 @@
 	/**
 	 * Register the plugin
 	 */
-	$cssp->register_plugin('before_compile', 0, 'ie6enhancements');
+	$cssp->register_plugin('before_compile', 0, 'ieenhancements');
 
 
 ?>
