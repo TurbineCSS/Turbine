edc6015e77e8bcbabe105d62761a5961172415c6
Peter Kröner <peter@peterkroener.de>
2010-05-21 21:39:35 +0200

    Fixed issue #58: division by zero in box-shadow when offset is 0 pixel
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- plugins/boxshadow.php
+++ plugins/boxshadow.php
@@ -53,7 +53,7 @@
 		$value = $cssp->get_final_value($values);
 		$filter_properties = array();
 		// Build the filter value
-		if(preg_match('/([0-9]+)\D+([0-9]+)\D+([0-9]+)\D+#([0-9A-F]{3,6})+/i', trim($value), $matches) == 1){
+		if(preg_match('/([-0-9]+)\D+([-0-9]+)\D+([-0-9]+)\D+#([0-9A-F]{3,6})+/i', trim($value), $matches) == 1){
 			$xoffset = intval($matches[1]);
 			$yoffset = intval($matches[2]);
 			$blur = intval($matches[3]);
@@ -61,41 +61,11 @@
 			if(strlen($color) == 3){
 				$color = substr($color,0,1).substr($color,0,1).substr($color,1,1).substr($color,1,1).substr($color,2,1).substr($color,2,1);
 			}
-			$median_offset = round(($xoffset + $yoffset) / 2);
+			$median_offset = round((abs($xoffset) + abs($yoffset)) / 2);
 			$opacity = (($median_offset - $blur) > 0) ? (($median_offset - $blur) / $median_offset) : 0.05;
 			$color_opacity = strtoupper(str_pad(dechex(round(hexdec(substr($color,0,2)) * $opacity)), 2, '0', STR_PAD_LEFT).str_pad(dechex(round(hexdec(substr($color,2,2)) * $opacity)),2,'0',STR_PAD_LEFT).str_pad(dechex(round(hexdec(substr($color,4,2)) * $opacity)),2,'0',STR_PAD_LEFT));
-			$direction = 135;
-			$direction_factor = abs($xoffset) / abs($yoffset);
-			if($direction_factor == 1){
-				if($xoffset > 0 && $yoffset > 0){
-					$direction = 135;
-				}
-				elseif($xoffset > 0 && $yoffset < 0){
-					$direction = 45;
-				}
-				elseif($xoffset < 0 && $yoffset > 0){
-					$direction = 315;
-				}
-				else{
-					$direction = 225;
-				}
-			}
-			elseif($direction_factor > 1){
-				if($xoffset > 0){
-					$direction = 90;
-				}
-				else{
-					$direction = 270;
-				}
-			}
-			else{
-				if($yoffset > 0){
-					$direction = 180;
-				}
-				else{
-					$direction = 0;
-				}
-			}
+			// Calculate direction
+			$direction = rad2deg(atan2($yoffset, $xoffset * -1));
 			// Hard Shadow
 			if($blur == 0){
 				$filter = 'progid:DXImageTransform.Microsoft.dropshadow(OffX='.$xoffset.',OffY='.$yoffset.',Color=\'#'.strtoupper(str_pad(dechex(round($opacity * 255)),2,'0',STR_PAD_LEFT)).$color.'\',Positive=\'true\')';
@@ -104,9 +74,9 @@
 			else{
 				$filter = 'progid:DXImageTransform.Microsoft.Shadow(Color=\'#'.$color.'\',Direction='.$direction.',Strength='.$median_offset.')';
 			}
-			//IE8-compliance (note: value inside apostrophes!)
+			// IE8 compliance (note: value inside apostrophes!)
 			$filter_properties['-ms-filter'] = array('"'.$filter.'"');
-			//Legacy IE-compliance
+			// Legacy IE compliance
 			$filter_properties['filter'] = array($filter);
 			$filter_properties['zoom'] = array('1');
 		}
