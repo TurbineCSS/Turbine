74dff1413a6d158a186a46cb306a00d2b53c5c59
Peter Kröner <peter@peterkroener.de>
2010-06-15 13:41:30 +0200

    Minifier now turns hex colors to shorter named colors when possible
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- plugins/minifier.php
+++ plugins/minifier.php
@@ -7,14 +7,14 @@
 	 * Usage:   Nobrainer, just switch it on
 	 * 
 	 * Status:  Stable
-	 * Version: 1.2
+	 * Version: 1.3
 	 * @param mixed &$parsed
 	 * @return void
 	 */
 	function minifier(&$parsed){
 		global $browser, $cssp;
 		// For shortening colors
-		$color_pattern = '/\#([A-F0-9])\1([A-F0-9])\2([A-F0-9])\3\b/i';
+		$color_shortable_pattern = '/\#([A-F0-9])\1([A-F0-9])\2([A-F0-9])\3\b/i';
 		$color_properties = array(
 			'color',
 			'background',
@@ -26,6 +26,26 @@
 			'border-bottom',
 			'border-right',
 			'box-shadow'
+		);
+		// Named colors with a length shorter than their hex equivalent
+		$color_pattern = '/(?:^|\b)(#[A-F0-9]{6})(?:$|\b)/i';
+		$color_names = array(
+			'#CD853F' => 'peru',
+			'#FFD700' => 'gold',
+			'#D2B48C' => 'tan',
+			'#FFC0CB' => 'pink',
+			'#DDA0DD' => 'plum',
+			'#000080' => 'navy',
+			'#008080' => 'teal',
+			'#808000' => 'olive',
+			'#008000' => 'green',
+			'#FFFAFA' => 'snow',
+			'#F0FFFF' => 'azure',
+			'#FFFFF0' => 'ivory',
+			'#FAF0E6' => 'linen',
+			'#F5F5DC' => 'beige',
+			'#F5DEB3' => 'wheat',
+			'#808080' => 'gray'
 		);
 		// Comma-sepparated properties
 		$tokenized_properties = array(
@@ -47,7 +67,14 @@
 						foreach($parsed[$block][$selector][$property] as $key => $value){
 							// Optimize hex colors
 							if(in_array($property, $color_properties)){
-								$parsed[$block][$selector][$property][$key] = preg_replace($color_pattern, '#\1\2\3', $value);
+								if(preg_match($color_shortable_pattern, $value)){
+									$parsed[$block][$selector][$property][$key] = preg_replace($color_shortable_pattern, '#\1\2\3', $value);
+								}
+								elseif(preg_match($color_pattern, $value, $color_matches)){
+									if(array_key_exists($color_matches[1], $color_names)){
+										$parsed[$block][$selector][$property][$key] = preg_replace($color_pattern, $color_names[$color_matches[1]], $value);
+									}
+								}
 							}
 							// Optimize tokenized strings
 							if(in_array($property, $tokenized_properties)){
@@ -59,7 +86,7 @@
 								$parsed[$block][$selector][$property][$key] = preg_replace($float_pattern, '\2\3\4', $parsed[$block][$selector][$property][$key]);
 							}
 							// Shorten long margins and paddings
-							if($property == 'margin'/* || $property == 'padding'*/){
+							if($property == 'margin' || $property == 'padding'){
 								preg_match_all('/((?:\b|-)[0-9.]*(?:em|ex|px|in|cm|mm|pt|pc)?\b)/', $value, $matches);
 								// Filter out empty values
 								$matches[0] = array_values(array_filter($matches[0], create_function('$element', 'return strlen($element) > 0;')));
