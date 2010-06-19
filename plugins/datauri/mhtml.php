94c5d3ecd914498e2953ee2c6ed95186adac125f
Peter Kröner <peter@peterkroener.de>
2010-06-20 01:53:11 +0200

    License info for mhtml.php
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- plugins/datauri/mhtml.php
+++ plugins/datauri/mhtml.php
@@ -1,15 +1,27 @@
 <?php
-	if(isset($_GET['cache'])){
-		$mhtmlfile = '../../cache/'.preg_replace('/[^a-z0-9]/i','',preg_replace('/\!img[a-z0-9]+$/i','',$_GET['cache'])).'_mhtml.txt';
-		$etag = md5($mhtmlfile.filemtime($mhtmlfile));
-		if(@$_SERVER['HTTP_IF_NONE_MATCH'] === $etag){
-			header('HTTP/1.1 304 Not Modified');
-			exit();
-		}
-		header("Cache-Control: no-cache, must-revalidate");
-		header("Expires: ".gmdate('D, d M Y H:i:s')." GMT");
-		header("Content-type: text/plain"); 
-		header("ETag: ".$etag);
-		echo file_get_contents($mhtmlfile);
+
+/**
+ * This file is part of Turbine
+ * http://github.com/SirPepe/Turbine
+ * 
+ * Copyright Peter Kröner
+ * Licensed under GNU LGPL 3, see license.txt or http://www.gnu.org/licenses/
+ */
+
+
+if(isset($_GET['cache'])){
+	$mhtmlfile = '../../cache/'.preg_replace('/[^a-z0-9]/i','',preg_replace('/\!img[a-z0-9]+$/i','',$_GET['cache'])).'_mhtml.txt';
+	$etag = md5($mhtmlfile.filemtime($mhtmlfile));
+	if(@$_SERVER['HTTP_IF_NONE_MATCH'] === $etag){
+		header('HTTP/1.1 304 Not Modified');
+		exit();
 	}
+	header("Cache-Control: no-cache, must-revalidate");
+	header("Expires: ".gmdate('D, d M Y H:i:s')." GMT");
+	header("Content-type: text/plain"); 
+	header("ETag: ".$etag);
+	echo file_get_contents($mhtmlfile);
+}
+
+
 ?>
