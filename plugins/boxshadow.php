f001df2ed567bdb490fcd5c5784e9dbfbc5e2e74
Peter Kröner <peter@peterkroener.de>
2010-08-08 20:44:04 +0200

    Turns out the box shadow plugin didn't support "none" as a value for IE
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- plugins/boxshadow.php
+++ plugins/boxshadow.php
@@ -61,7 +61,18 @@ function boxshadow_filters($values){
 	$value = $cssp->get_final_value($values);
 	$filter_properties = array();
 	// Build the filter value
-	if(preg_match('/([-0-9]+)\D+([-0-9]+)\D+([-0-9]+)\D+#([0-9A-F]{3,6})+/i', $value, $matches) == 1){
+	if($value == 'none'){
+		$filters = array(
+			'progid:DXImageTransform.Microsoft.dropshadow(enabled:false)',
+			'progid:DXImageTransform.Microsoft.Shadow(enabled:false)'
+		);
+		// IE8 compliance (note: value inside apostrophes!)
+		$filter_properties['-ms-filter'] = $filters;
+		// Legacy IE compliance
+		$filter_properties['filter'] = $filters;
+		$filter_properties['zoom'] = array('1');
+	}
+	elseif(preg_match('/([-0-9]+)\D+([-0-9]+)\D+([-0-9]+)\D+#([0-9A-F]{3,6})+/i', $value, $matches) == 1){
 		$xoffset = intval($matches[1]);
 		$yoffset = intval($matches[2]);
 		$blur = intval($matches[3]);
