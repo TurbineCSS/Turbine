00591b74a3dc170a65a431602581eca07f000796
Peter Kröner <peter@peterkroener.de>
2010-08-14 19:02:19 +0200

    Added "no IE" option for the background gradient plugin
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- plugins/backgroundgradient.php
+++ plugins/backgroundgradient.php
@@ -29,6 +29,7 @@
  */
 function backgroundgradient(&$parsed){
 	global $cssp, $browser;
+	$settings = Plugin::get_settings('backgroundgradient');
 	// Searches for W3C-style two-stepped linear gradient
 	$urlregex = '/linear-gradient\s*\(\s*(top|left)\s*,\s*(#[0-9A-F]+|rgba*\([0-9,]+\))\s*,\s*(#[0-9A-F]+|rgba*\([0-9,]+\))\s*\)/i';
 	// In which properties to search
@@ -104,78 +105,82 @@ function backgroundgradient(&$parsed){
 							} // End switch
 
 							// Use filter fallbacks in IE
-							$filter_properties = array();
-							if(strtolower($matches[1]) == 'top'){
-								$ie_gradienttype = '0';
-							}
-							else{
-								$ie_gradienttype = '1';
-							}
-							// Expand shorthand colors
-							$shorthandpattern = '/^#([0-9A-F]{1})([0-9A-F]{1})([0-9A-F]{1})$/i';
-							if(preg_match($shorthandpattern,$matches[2],$shorthandmatches)){
-								$matches[2] = '#FF'.strtoupper($shorthandmatches[1].$shorthandmatches[1].$shorthandmatches[2].$shorthandmatches[2].$shorthandmatches[3].$shorthandmatches[3]);
-							}
-							if(preg_match($shorthandpattern,$matches[3],$shorthandmatches)){
-								$matches[3] = '#FF'.strtoupper($shorthandmatches[1].$shorthandmatches[1].$shorthandmatches[2].$shorthandmatches[2].$shorthandmatches[3].$shorthandmatches[3]);
-							}
-							// Convert from RGB colors
-							$rgbpattern = '/rgb\([\s]*(.+?)[\s]*,[\s]*(.+?)[\s]*,[\s]*(.+?)[\s]*\)/i';
-							if(preg_match($rgbpattern,$matches[2],$rgbmatches)){
-								$matches[2] = '#FF'.strtoupper(dechex(intval($rgbmatches[1])).dechex(intval($rgbmatches[2])).dechex(intval($rgbmatches[3])));
-							}
-							if(preg_match($rgbpattern,$matches[3],$rgbmatches)){
-								$matches[3] = '#FF'.strtoupper(dechex(intval($rgbmatches[1])).dechex(intval($rgbmatches[2])).dechex(intval($rgbmatches[3])));
-							}
-							// Convert from RGBA colors
-							$rgbapattern = '/rgba\([\s]*(.+?)[\s]*,[\s]*(.+?)[\s]*,[\s]*(.+?)[\s]*,[\s]*(.+?)[\s]*\)/i';
-							if(preg_match($rgbapattern,$matches[2],$rgbamatches)){
-								$matches[2] = '#'.strtoupper(dechex(intval(floatval($rgbamatches[4]) * 255)).dechex(intval($rgbamatches[1])).dechex(intval($rgbamatches[2])).dechex(intval($rgbamatches[3])));
-							}
-							if(preg_match($rgbapattern,$matches[3],$rgbamatches)){
-								$matches[3] = '#'.strtoupper(dechex(intval(floatval($rgbamatches[4]) * 255)).dechex(intval($rgbamatches[1])).dechex(intval($rgbamatches[2])).dechex(intval($rgbamatches[3])));
-							}
-							// TODO: Convert from HSL colors. See colormodels for converting algorithm
-							// TODO: Convert from HSLA colors. See colormodels for converting algorithm
-							$filter = 'progid:DXImageTransform.Microsoft.gradient(startColorstr='.$matches[2].',endColorstr='.$matches[3].',gradientType='.$ie_gradienttype.')';
-							// Legacy IE compliance
-							$filter_properties['filter'] = array($filter);
-							// IE8 compliance
-							$filter_properties['-ms-filter'] = array($filter);
-							// Salvage any background information that is NOT gradient stuff and preserve it in a form IE can handle
-							$background_rest = preg_replace(
-								'/(-moz-|-webkit-)*linear-gradient\s*\(.*?\)/i',
-								'',
-								$parsed[$block][$selector][$property][$i]
-							);
-							$background_rest = trim($background_rest);
-							if($background_rest != ''){
-								$filter_properties['*background'] = array($background_rest);            // IE7 and 6
-								$filter_properties['background /*\**/'] = array($background_rest.'\9'); // IE8
-							}
-							// hasLayout stuff
-							$filter_properties['zoom'] = array('1');
-							// Insert all
-							$cssp->insert_properties($filter_properties, $block, $selector, NULL, $property);
-							foreach($filter_properties as $filter_property => $filter_value){
-								CSSP::comment($parsed[$block][$selector], $filter_property, 'Added by background-gradient plugin');
-							}
+							if(!in_array('noie', $settings)){
+								$filter_properties = array();
+								if(strtolower($matches[1]) == 'top'){
+									$ie_gradienttype = '0';
+								}
+								else{
+									$ie_gradienttype = '1';
+								}
+								// Expand shorthand colors
+								$shorthandpattern = '/^#([0-9A-F]{1})([0-9A-F]{1})([0-9A-F]{1})$/i';
+								if(preg_match($shorthandpattern,$matches[2],$shorthandmatches)){
+									$matches[2] = '#FF'.strtoupper($shorthandmatches[1].$shorthandmatches[1].$shorthandmatches[2].$shorthandmatches[2].$shorthandmatches[3].$shorthandmatches[3]);
+								}
+								if(preg_match($shorthandpattern,$matches[3],$shorthandmatches)){
+									$matches[3] = '#FF'.strtoupper($shorthandmatches[1].$shorthandmatches[1].$shorthandmatches[2].$shorthandmatches[2].$shorthandmatches[3].$shorthandmatches[3]);
+								}
+								// Convert from RGB colors
+								$rgbpattern = '/rgb\([\s]*(.+?)[\s]*,[\s]*(.+?)[\s]*,[\s]*(.+?)[\s]*\)/i';
+								if(preg_match($rgbpattern,$matches[2],$rgbmatches)){
+									$matches[2] = '#FF'.strtoupper(dechex(intval($rgbmatches[1])).dechex(intval($rgbmatches[2])).dechex(intval($rgbmatches[3])));
+								}
+								if(preg_match($rgbpattern,$matches[3],$rgbmatches)){
+									$matches[3] = '#FF'.strtoupper(dechex(intval($rgbmatches[1])).dechex(intval($rgbmatches[2])).dechex(intval($rgbmatches[3])));
+								}
+								// Convert from RGBA colors
+								$rgbapattern = '/rgba\([\s]*(.+?)[\s]*,[\s]*(.+?)[\s]*,[\s]*(.+?)[\s]*,[\s]*(.+?)[\s]*\)/i';
+								if(preg_match($rgbapattern,$matches[2],$rgbamatches)){
+									$matches[2] = '#'.strtoupper(dechex(intval(floatval($rgbamatches[4]) * 255)).dechex(intval($rgbamatches[1])).dechex(intval($rgbamatches[2])).dechex(intval($rgbamatches[3])));
+								}
+								if(preg_match($rgbapattern,$matches[3],$rgbamatches)){
+									$matches[3] = '#'.strtoupper(dechex(intval(floatval($rgbamatches[4]) * 255)).dechex(intval($rgbamatches[1])).dechex(intval($rgbamatches[2])).dechex(intval($rgbamatches[3])));
+								}
+								// TODO: Convert from HSL colors. See colormodels for converting algorithm
+								// TODO: Convert from HSLA colors. See colormodels for converting algorithm
+								$filter = 'progid:DXImageTransform.Microsoft.gradient(startColorstr='.$matches[2].',endColorstr='.$matches[3].',gradientType='.$ie_gradienttype.')';
+								// Legacy IE compliance
+								$filter_properties['filter'] = array($filter);
+								// IE8 compliance
+								$filter_properties['-ms-filter'] = array($filter);
+								// Salvage any background information that is NOT gradient stuff and preserve it in a form IE can handle
+								$background_rest = preg_replace(
+									'/(-moz-|-webkit-)*linear-gradient\s*\(.*?\)/i',
+									'',
+									$parsed[$block][$selector][$property][$i]
+								);
+								$background_rest = trim($background_rest);
+								if($background_rest != ''){
+									$filter_properties['*background'] = array($background_rest);            // IE7 and 6
+									$filter_properties['background /*\**/'] = array($background_rest.'\9'); // IE8
+								}
+								// hasLayout stuff
+								$filter_properties['zoom'] = array('1');
+								// Insert all
+								$cssp->insert_properties($filter_properties, $block, $selector, NULL, $property);
+								foreach($filter_properties as $filter_property => $filter_value){
+									CSSP::comment($parsed[$block][$selector], $filter_property, 'Added by background-gradient plugin');
+								}
+							} // End if(!in_array('noie', $settings))
 
 						}
 						else{
-							// This is fucking ugly, but must be done to keep thing sane for css developers. In order to have background
-							// declarations that don't contain any gradient _remove_ gradients that the element may have inherited
+							// This is fucking ugly, but must be done to keep things sane for css developers. In order to have background
+							// declarations that don't contain any gradient _remove_ gradients that the element may have inherited,
 							// we have to explicitly add disabled filters for IE - always. So in the end what this does is adding three
 							// lines of css code for every background property as long as it doesn't add a gradient. Talk about bloated
 							// code... but hey, blame the IE team for their fucked up browsers, not us...
-							$filter_properties = array();
-							$filter = 'progid:DXImageTransform.Microsoft.gradient(enabled:false)';
-							$filter_properties['zoom'] = array('1');
-							$filter_properties['filter'] = array($filter);
-							$filter_properties['-ms-filter'] = array($filter);
-							$cssp->insert_properties($filter_properties, $block, $selector, NULL, $property);
-							foreach($filter_properties as $filter_property => $filter_value){
-								CSSP::comment($parsed[$block][$selector], $filter_property, 'Added by background-gradient plugin');
+							if(!in_array('noie', $settings)){
+								$filter_properties = array();
+								$filter = 'progid:DXImageTransform.Microsoft.gradient(enabled:false)';
+								$filter_properties['zoom'] = array('1');
+								$filter_properties['filter'] = array($filter);
+								$filter_properties['-ms-filter'] = array($filter);
+								$cssp->insert_properties($filter_properties, $block, $selector, NULL, $property);
+								foreach($filter_properties as $filter_property => $filter_value){
+									CSSP::comment($parsed[$block][$selector], $filter_property, 'Added by background-gradient plugin');
+								}
 							}
 						}
 					}
