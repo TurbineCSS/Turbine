a2cada0901de4f4eeca98ad8462bbdbeef90e715
Peter Kröner <peter@peterkroener.de>
2010-08-19 13:49:14 +0200

    Added utility class (Shared functionality for plugins and core)
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

---
+++ lib/utility.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of Turbine
+ * http://github.com/SirPepe/Turbine
+ * 
+ * Copyright Peter Kröner
+ * Licensed under GNU LGPL 3, see license.txt or http://www.gnu.org/licenses/
+ */
+
+
+/**
+ * Utility
+ * Shared functionality for plugins and core
+ */
+class Utility {
+
+
+/*
+ * hex2rgb
+ * Convert hex colors RGB(A)
+ * @param string $input
+ * @param int $alpha Return RGBA? 0 = No, 1 = Auto (return alpha only if the input uses an alpha value), 2 = Yes
+ * @return array $output
+ */
+static function hex2rgb($input, $alpha = 1){}
+
+
+/*
+ * rgb2hex
+ * Convert hex colors RGB(A)
+ * @param string $input
+ * @param int $alpha Return hex with alpha? 0 = No, 1 = Auto (return alpha only if the input uses an alpha value), 2 = Yes
+ * @return array $output
+ */
+static function rgb2hex($input, $alpha = 1){}
+
+
+}
+
+
+?>
