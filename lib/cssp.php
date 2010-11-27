721f4325e9e11cc0320746710e08b499a330c39d
Peter Kröner <peter@peterkroener.de>
2010-11-27 14:25:31 +0100

    Implemented extender. Still buggy.
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- lib/cssp.php
+++ lib/cssp.php
@@ -36,12 +36,69 @@ class Cssp extends Parser2 {
 	 * @return void
 	 */
 	public function compile(){
+		$this->apply_extender();
 		$this->apply_aliases();
 		$this->apply_property_expansion();
 		$this->apply_inheritance();
 		$this->apply_copying();
 		$this->apply_constants();
 		$this->cleanup();
+	}
+
+
+	/**
+	 * apply_extender
+	 * Applies selector extender logic
+	 * @return void
+	 */
+	public function apply_extender(){
+		foreach($this->parsed as $block => $css) {
+			foreach($this->parsed[$block] as $selector => $styles){
+				$tokenized = $this->tokenize($selector, array('"' ,"'", ','));
+				// Define new and extended selector
+				$extended_selector = null;
+				$new_selector = $selector;
+				foreach($tokenized as $key => $token){
+					// Looking for &.class, &#id or &:selector
+					if(preg_match_all('@(\&.|&#|&\:)@', $token, $matches)){
+						// Remove the &
+						$new_selector = preg_replace('@( \&)@', '', $selector);
+						$extended_selector = '';
+					}
+					// Looking for auto generated selector i.e. "a(:link, :visited)"
+					elseif(preg_match_all('@(.*?)\((\:.*?)\)($| .*?$)@', $token, $matches)){
+						$exploded_selectors = explode(',',$matches[2][0]);
+						foreach($exploded_selectors as $key => $value){
+							$extended_selector .= preg_replace('@\((\:.*?)\)@', trim($value), $token) . ", ";
+						}
+						$extended_selector = preg_replace('@(, )$@', '', $extended_selector);
+						$new_selector = str_replace($matches[0][0], $extended_selector, $new_selector);
+						$extended_selector = '';
+					}
+					// Looking for auto generated selector i.e. "div.foo(1-3)"
+					elseif (preg_match_all('@(.*?)\((\d{1,})-(\d{1,})\)@', $token, $matches)){
+						// Check if starting value is smaller than ending value - "div.foo(3-1)" i.e. will be ignored
+						if($matches[2][0] < $matches[3][0]){
+							for($i=$matches[2][0]; $i <= $matches[3][0]; $i++){
+								$extended_selector .= preg_replace('@\((\d{1,})-(\d{1,})\)@', $i, $token) . ", ";
+							}
+							$extended_selector = preg_replace('@(, )$@', '', $extended_selector);
+							$new_selector = str_replace($matches[0][0], $extended_selector, $new_selector);
+							$extended_selector = '';
+						}
+					}
+				}
+				// Insert the new selector if present
+				if(isset($extended_selector)){
+					// Remove ', ' at the end of the new selector if present
+					$changed = array();
+					$changed[$new_selector] = $styles;
+					$this->insert($changed, $block, $selector);
+					// Remove old selector
+					unset($this->parsed[$block][$selector]);
+				}
+			}
+		}
 	}
 
 
