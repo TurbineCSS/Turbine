2707ba7af0c4ca12fb1a9e40c0d2da8ccc186d17
Tomasz Janusz <tomick@tomick-pc.(none)>
2011-01-09 00:04:35 +0100

    Do not render IE fixes in other browsers.

--- plugins/bugfixes.php
+++ plugins/bugfixes.php
@@ -25,17 +25,24 @@ function bugfixes(&$parsed){
 	global $cssp, $browser;
 	$changed = array();
 
-	// IE6: Image margin bottom bug
-	$changed['img']['vertical-align'][] = 'bottom';
-	// IE: remove scrollbars from textareas
-	$changed['textarea']['overflow'][] = 'auto';
+    if($browser->browser == 'ie' && floatval($browser->browser_version) == 6){
+        // IE6: Image margin bottom bug
+	    $changed['img']['vertical-align'][] = 'bottom';
+        
+        // IE6: Background image flickers on hover
+        $changed['html']['filter'][] = 'expression(document.execCommand("BackgroundImageCache",false,true))';
 
-	// IE6: Background image flickers on hover
-	$changed['html']['filter'][] = 'expression(document.execCommand("BackgroundImageCache",false,true))';
+        // IE6: Fix transparent PNGs, see http://www.twinhelix.com/css/iepngfix/
+        $htc_path = rtrim(dirname($_SERVER['SCRIPT_NAME']),'/').'/plugins/bugfixes/iepngfix.htc';
+        $changed['img']['behavior'][] = 'url("'.$htc_path.'")';
 
-	// IE6: Fix transparent PNGs, see http://www.twinhelix.com/css/iepngfix/
-	$htc_path = rtrim(dirname($_SERVER['SCRIPT_NAME']),'/').'/plugins/bugfixes/iepngfix.htc';
-	$changed['img']['behavior'][] = 'url("'.$htc_path.'")';
+    }
+    if($browser->browser == 'ie'){
+        // IE: remove scrollbars from textareas
+	    $changed['textarea']['overflow'][] = 'auto';
+
+    }
+
 
 	// IE6 and 7: resample images bicubic instead of using nearest neighbor method
 	if($browser->browser == 'ie' && floatval($browser->browser_version) < 7){
@@ -51,21 +58,28 @@ function bugfixes(&$parsed){
 		
 		// IE6: Input align, see http://tjkdesign.com/ez-css/css/base.css
 		$changed['img']['vertical-align'][] = 'text-bottom';
-	}	
-	// IE 7: resample images bicubic instead of using nearest neighbor method
-	$changed['img']['-ms-interpolation-mode'][] = 'bicubic';
 
-	// IE6 and 7: Enable full styleability for buttons, see http://www.sitepoint.com/forums/showthread.php?t=547059
-	$changed['button']['overflow'][] = 'visible';
-	$changed['button']['width'][] = 'auto';
-	$changed['button']['white-space'][] = 'nowrap';
 
-	// IE6 and 7: Missing :hover-property on every tag except a, see http://www.xs4all.nl/~peterned/csshover.html
-	$htc_path = rtrim(dirname($_SERVER['SCRIPT_NAME']),'/').'/plugins/bugfixes/csshover3.htc';
-	$changed['body']['behavior'][] = 'url("'.$htc_path.'")';
-	// IE8: Reenable cleartype where filters are set
-	$htc_path = rtrim(dirname($_SERVER['SCRIPT_NAME']),'/').'/plugins/bugfixes/';
-	$changed['body']['behavior'][] = 'url("'.$htc_path.'csshover3.htc") url("'.$htc_path.'cleartypefix.htc")';
+        // IE6 and 7: Enable full styleability for buttons, see http://www.sitepoint.com/forums/showthread.php?t=547059
+        $changed['button']['overflow'][] = 'visible';
+        $changed['button']['width'][] = 'auto';
+        $changed['button']['white-space'][] = 'nowrap';
+
+        // IE6 and 7: Missing :hover-property on every tag except a, see http://www.xs4all.nl/~peterned/csshover.html
+        $htc_path = rtrim(dirname($_SERVER['SCRIPT_NAME']),'/').'/plugins/bugfixes/csshover3.htc';
+        $changed['body']['behavior'][] = 'url("'.$htc_path.'")';
+	}
+
+    if($browser->browser == 'ie' && floatval($browser->browser_version) == 7){
+        // IE 7: resample images bicubic instead of using nearest neighbor method
+	    $changed['img']['-ms-interpolation-mode'][] = 'bicubic';
+    }
+
+    if($browser->browser == 'ie' && floatval($browser->browser_version) == 8){
+        // IE8: Reenable cleartype where filters are set
+	    $htc_path = rtrim(dirname($_SERVER['SCRIPT_NAME']),'/').'/plugins/bugfixes/';
+	    $changed['body']['behavior'][] = 'url("'.$htc_path.'csshover3.htc") url("'.$htc_path.'cleartypefix.htc")';
+    }
 
 	// Firefox: Ghost margin around buttons, see http://www.sitepoint.com/forums/showthread.php?t=547059
 	$changed['button::-moz-focus-inner']['padding'][] = '0';
